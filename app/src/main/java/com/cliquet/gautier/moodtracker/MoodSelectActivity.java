a9aab8c460f5a120f3c333385f671ce1ab8a568d
Ocelot <gcliquet@gmail.com>
2018-10-22 20:36:26 +0200

    Trying to modify background from each mood based on user interaction(doesn't work, not ask in the project specification)

--- app/src/main/java/com/cliquet/gautier/moodtracker/MoodSelectActivity.java
+++ app/src/main/java/com/cliquet/gautier/moodtracker/MoodSelectActivity.java
@@ -1,23 +1,43 @@
 package com.cliquet.gautier.moodtracker;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MoodSelectActivity extends AppCompatActivity {
 
-    ImageView selectedMood[] = new ImageView[5];
+    //must be erased: Using EditText to display some variables(for test only)
+    private TextView mTestText;
+
+    private ImageView selectedMood[] = new ImageView[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_select);
 
+        mTestText = findViewById(R.id.activity_mood_testText_text);
+
         selectedMood[4] = findViewById(R.id.activity_mood_select_superHappy_image);
         selectedMood[3] = findViewById(R.id.activity_mood_select_happy_image);
         selectedMood[2] = findViewById(R.id.activity_mood_select_normal_image);
         selectedMood[1] = findViewById(R.id.activity_mood_select_disappointed_image);
         selectedMood[0] = findViewById(R.id.activity_mood_select_sad_image);
+
+        selectedMood[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //final Drawable color = selectedMood[2].getBackground();
+                //ColorDrawable imageColor = (ColorDrawable) selectedMood[2].getBackground();
+                //int colorId = imageColor.getColor();
+
+                selectedMood[2].setBackgroundColor(0xFFAA5599);
+                //mTestText.setText(String.valueOf(colorId));
+            }
+        });
     }
 }
