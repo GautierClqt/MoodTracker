c215dbae2dfb9c1112d023bf49be347b5ec0d18e
Ocelot <gcliquet@gmail.com>
2018-10-23 17:21:19 +0200

    code has been redo to the ground - users can cycle through moods by clicking

--- app/src/main/java/com/cliquet/gautier/moodtracker/MoodSelectActivity.java
+++ app/src/main/java/com/cliquet/gautier/moodtracker/MoodSelectActivity.java
@@ -1,43 +1,62 @@
 package com.cliquet.gautier.moodtracker;
 
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-
+import android.widget.RelativeLayout;
 
 public class MoodSelectActivity extends AppCompatActivity {
 
-    //must be erased: Using EditText to display some variables(for test only)
-    private TextView mTestText;
-
-    private ImageView selectedMood[] = new ImageView[5];
+    //i is used to cycle through moods, is set to 2 in order to display the normal mood by default
+    public int i = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_select);
 
-        mTestText = findViewById(R.id.activity_mood_testText_text);
+        //connecting the views
+        final ImageView mDisplayedMood;
+        mDisplayedMood = findViewById(R.id.activity_mood_select_placeholder_image);
 
-        selectedMood[4] = findViewById(R.id.activity_mood_select_superHappy_image);
-        selectedMood[3] = findViewById(R.id.activity_mood_select_happy_image);
-        selectedMood[2] = findViewById(R.id.activity_mood_select_normal_image);
-        selectedMood[1] = findViewById(R.id.activity_mood_select_disappointed_image);
-        selectedMood[0] = findViewById(R.id.activity_mood_select_sad_image);
+        final RelativeLayout mLayout;
+        mLayout = findViewById(R.id.activity_mood_select_layout_layout);
 
-        selectedMood[2].setOnClickListener(new View.OnClickListener() {
+        //all five moods' images are stored in this array
+        final Drawable[] moodList = new Drawable[5];
+        moodList[0] = getResources().getDrawable(R.drawable.smiley_sad);
+        moodList[1] = getResources().getDrawable(R.drawable.smiley_disappointed);
+        moodList[2] = getResources().getDrawable(R.drawable.smiley_normal);
+        moodList[3] = getResources().getDrawable(R.drawable.smiley_happy);
+        moodList[4] = getResources().getDrawable(R.drawable.smiley_super_happy);
+
+        //normal mood and background are set by default
+        mDisplayedMood.setImageDrawable(moodList[2]);
+
+        //at each click on the ImageView moods and the layout background colors are cycle through(worst to best)
+        mDisplayedMood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //final Drawable color = selectedMood[2].getBackground();
-                //ColorDrawable imageColor = (ColorDrawable) selectedMood[2].getBackground();
-                //int colorId = imageColor.getColor();
-
-                selectedMood[2].setBackgroundColor(0xFFAA5599);
-                //mTestText.setText(String.valueOf(colorId));
+                i++;
+                mDisplayedMood.setImageDrawable(moodList[i]);   //display the next mood
+                //display the corresponding background color
+                switch (i) {
+                    case 0: mLayout.setBackgroundColor(0xffde3c50);
+                            break;
+                    case 1: mLayout.setBackgroundColor(0xff9b9b9b);
+                            break;
+                    case 2: mLayout.setBackgroundColor(0xa5468ad9);
+                            break;
+                    case 3: mLayout.setBackgroundColor(0xffb8e986);
+                            break;
+                    case 4: mLayout.setBackgroundColor(0xfff9ec4f);
+                            i = -1; //will be valued to 0(worst mood) at the next click
+                            break;
+                }
             }
         });
     }
 }
+
