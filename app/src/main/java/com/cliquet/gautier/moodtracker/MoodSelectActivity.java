d2a7ff01a084f46958c8ed2213ab8d368dc9db4c
Ocelot <gcliquet@gmail.com>
2018-10-29 11:37:01 +0100

    Comments edit and save are working - code needs to be refactor

--- app/src/main/java/com/cliquet/gautier/moodtracker/MoodSelectActivity.java
+++ app/src/main/java/com/cliquet/gautier/moodtracker/MoodSelectActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MoodSelectActivity extends AppCompatActivity {
+public class MoodSelectActivity extends AppCompatActivity implements CommentDialog.CommentDialogListener {
 
-    public RelativeLayout mLayout;
-    public Drawable[] moodList = new Drawable[5];
-    public int moodIndex;
-    public int moodPreferences;
+    private RelativeLayout mLayout;
+    private Drawable[] moodList = new Drawable[5];
+    private int moodIndex;
+    private int moodPreferences;
+    private String commentPreferences;
+    private TextView mDisplayComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +25,20 @@ public class MoodSelectActivity extends AppCompatActivity {
 
         final SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
-        //preferences.edit().putInt("mood_index", moodIndex).apply(); // <- MUST BE ERASSED - use to set preferences right
+        //if there's no preferences normal mood is display by default
         moodIndex = getPreferences(MODE_PRIVATE).getInt("mood_index", 2);
+        commentPreferences = getPreferences(MODE_PRIVATE).getString("comment", "");
 
         //connecting the views
         final ImageView mDisplayedMood;
-        mDisplayedMood = findViewById(R.id.activity_mood_select_placeholder_image);
+        final ImageView mAddComment;
 
+        mDisplayedMood = findViewById(R.id.activity_mood_select_placeholder_image);
+        mAddComment = findViewById(R.id.activity_add_comment_image);
+        mDisplayComment = findViewById(R.id.activity_mood_select_comment_textview);
         mLayout = findViewById(R.id.activity_mood_select_layout_layout);
+
+        mDisplayComment.setText(commentPreferences);
 
         //all five moods' images are stored in moodList
         moodList[0] = getResources().getDrawable(R.drawable.smiley_sad);
@@ -41,28 +50,47 @@ public class MoodSelectActivity extends AppCompatActivity {
         //normal mood and background are set by default
         mDisplayedMood.setImageDrawable(moodList[moodIndex]);
         moodPreferences = modifyBackgroundColor(moodIndex);
-        //mLayout.setBackgroundColor(0xa5468ad9);
 
         //at each click on the ImageView moods and the layout background colors are cycle through(worst to best)
         mDisplayedMood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //to correctly cycle through the moods, especially to go from super happy mood to sad mood
                 if (moodIndex >= 0 && moodIndex < 4) {
                     moodIndex++;
                 }
-                else{
+                else{  //if moodIndex = 5(ie. out of moodList bound) it will be set back to 0
                     moodIndex = 0;
                 }
                 mDisplayedMood.setImageDrawable(moodList[moodIndex]);   //display the next mood
                 moodPreferences = modifyBackgroundColor(moodIndex);
                 preferences.edit().putInt("mood_index", moodPreferences).apply();
-                //savingPreferences(moodIndex);
+                commentPreferences = (String) mDisplayComment.getText();
+                preferences.edit().putString("comment", commentPreferences).apply();
+            }
+        });
+
+        mAddComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
             }
         });
     }
 
+    public void openDialog() {
+        CommentDialog commentDialog = new CommentDialog();
+        commentDialog.show(getSupportFragmentManager(), "comment dialog");
+    }
+
+    //display and save(preferences) the written comment
+    @Override
+    public void getComment(String comment) {
+        mDisplayComment.setText(comment);
+    }
+
+    //display the corresponding background color
     private int modifyBackgroundColor(int currentMood) {
-        //display the corresponding background color
         int saveMood = 0;
         switch (currentMood) {
             case 0: mLayout.setBackgroundColor(0xffde3c50);
@@ -79,7 +107,6 @@ public class MoodSelectActivity extends AppCompatActivity {
                 break;
             case 4: mLayout.setBackgroundColor(0xfff9ec4f);
                 saveMood = 4;
-                currentMood = -1; //will be valued to 0(worst mood) at the next click
                 break;
         }
         return saveMood;
